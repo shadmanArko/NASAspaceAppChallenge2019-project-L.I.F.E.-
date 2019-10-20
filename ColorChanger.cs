ab3a674e2c667c6f1c4ad33ec3eaa13c2bd04ab7
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2019-10-20 12:01:46 +0600

    Add files via upload

---
+++ ColorChanger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    public Material pinkMatirial;
+    public Material greyMatirial;
+    public Material whiteMatirial;
+    public Material blueMatirial;
+
+    public void SetPink()
+    {
+        SetMatirial(pinkMatirial);
+    }
+
+    public void SetGrey()
+    {
+        SetMatirial(greyMatirial);
+    }
+
+    public void SetWhite()
+    {
+        SetMatirial(whiteMatirial);
+    }
+
+    public void SetBlue()
+    {
+        SetMatirial(blueMatirial);
+    }
+
+    private void SetMatirial(Material newMatirial)
+    {
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+        renderer.material = newMatirial;
+    }
+}
---
+++ TreeGrowManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeGrowManager : MonoBehaviour
+{
+
+    public float maxSize;
+    public float growRate;
+    public float scale= 0.0f;
+    public Color[] colorVariation; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxSize = Random.Range(0.07f,0.085f);
+        growRate = Random.Range(0.02f, 0.08f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (scale < maxSize)
+        {
+            this.transform.localScale = Vector3.one * scale;
+            scale += growRate  *0.1f* Time.deltaTime;
+        }
+    }
+}
